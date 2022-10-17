1042079d05e0da7c7b5da1ff82d0e59dc608bbe2
Jackie <jye0325@gmail.com>
2022-10-17 18:55:37 -0400

    commit tree ae7baaef62261e2cdc51e1d75e9bcb39c779ec49

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :not_valid_titles
+    
+    PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def not_valid_titles
+        if PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "Invalid title")
+        end
+    end
 end
